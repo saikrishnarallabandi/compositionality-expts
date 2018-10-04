0966d30cc7e629638b9ea74432910c2019417512
Nidhi <nidhi0613@gamil.com>
2018-10-04 13:33:09 -0400

    Added pytorch lm

---
+++ explorations/lm/LM/model.py
@@ -0,0 +1,68 @@
+import torch.nn as nn
+from torch.autograd import Variable
+import torch
+class RNNModel(nn.Module):
+    """Container module with an encoder, a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False):
+        super(RNNModel, self).__init__()
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        if rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout=dropout)
+        else:
+            try:
+                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+            except KeyError:
+                raise ValueError( """An invalid option for `--model` was supplied,
+                                 options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
+            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        # Optionally tie weights as in:
+        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
+        # https://arxiv.org/abs/1608.05859
+        # and
+        # "Tying Word Vectors and Word Classifiers: A Loss Framework for Language Modeling" (Inan et al. 2016)
+        # https://arxiv.org/abs/1611.01462
+        if tie_weights:
+            if nhid != ninp:
+                raise ValueError('When using the tied flag, nhid must be equal to emsize')
+            self.decoder.weight = self.encoder.weight
+
+        self.init_weights()
+
+        self.rnn_type = rnn_type
+        self.nhid = nhid
+        self.nlayers = nlayers
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, hidden):
+        emb = self.drop(self.encoder(input))
+        output, hidden = self.rnn(emb, hidden)
+        output = self.drop(output)
+        #print("output after dropout")
+        decoded = self.decoder(output.view(output.size(0)*output.size(1), output.size(2)))
+        return decoded.view(output.size(0), output.size(1), decoded.size(1)), hidden
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters())
+        #print(type(weight))
+        temp1 = torch.nn.Parameter(torch.zeros(self.nlayers, bsz, self.nhid).cuda())
+        #temp = (Variable(torch.LongTensor(self.nlayers, bsz, self.nhid).fill_(0)).cuda(), Variable(torch.LongTensor(self.nlayers, bsz, self.nhid).fill_(0)).cuda())
+        temp2 = torch.nn.Parameter(torch.zeros(self.nlayers, bsz, self.nhid).cuda())
+	temp = (temp1, temp2)
+        #print (type(temp1))
+        if self.rnn_type == 'LSTM':
+            #print ("returned temp")
+            return temp
+            
+            #return (weight.new_zeros(self.nlayers, bsz, self.nhid),
+                   # weight.new_zeros(self.nlayers, bsz, self.nhid))
+        #else:
+        #   return weight.new_zeros(self.nlayers, bsz, self.nhid)
