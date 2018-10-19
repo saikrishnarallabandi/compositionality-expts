7f4c9e049bff9e7f7b5bbf9bd683c75b807a5e10
Nidhi <nidhi0613@gmail.com>
2018-10-19 17:53:38 -0400

    Add generation code for LM - RNN and VAE

--- explorations/lm/VAELM/generate_VAE.py
+++ explorations/lm/VAELM/generate_VAE.py
@@ -1,3 +1,5 @@
+# take <sos> tag and give to the latent and just generate from their
+
 
 
 
@@ -67,64 +69,57 @@ print len(corpus.train), "Batches for Train ||| ", len(corpus.train)*32, "Sample
 print len(corpus.valid), "Batches for Valid ||| ", len(corpus.valid)*32, "Samples of Valid"
 print len(corpus.test), "Samples for Test"
 
+
+
 ntokens = len(corpus.dictionary)
-model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.tied).cuda()
+
+model = model.VAEModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.tied).cuda()
 criterion = nn.CrossEntropyLoss(ignore_index=corpus.PAD_IDX)
+
 
 ###############################################################################
 # Generation code
 ###############################################################################
-
-
 
 def evaluate(data_source):
     # Turn on evaluation mode which disables dropout.
     model.eval()
     total_loss = 0.
     ntokens = len(corpus.dictionary)
-    #with torch.no_grad():
-    for i in range(0, len(data_source), args.bptt):
+    kl_loss = 0
+    ce_loss = 0
+
+    with torch.no_grad():
+      for i in range(0, len(data_source), args.bptt):
         data_full = data_source[i]
         data = data_full[:,0:data_full.size(1)-1]
         targets = data_full[:, 1:]
+        #hidden = model.init_hidden(data.size(0))
         hidden = None
         data = Variable(data).cuda()
         targets = Variable(targets).cuda()
-        #input_token  = corpus.dictionary.word2idx["<sos>"]
+
         original_questions = []
         gen_questions = []
         new_input_token = data[:,0].unsqueeze(1) # SOS
         for d in range(data.size(1)):
             original_questions.append(corpus.dictionary.idx2word[int(data[:,d])])
+
         while True:
-            #input_token = data[:,d].unsqueeze(1)
-            output, hidden = model(new_input_token, hidden)
-            ## SAMPLE
-            output = torch.nn.functional.softmax(output, dim=2)
+            recon_batch, _, _ = model(new_input_token, None)
+            output = torch.nn.functional.softmax(recon_batch, dim=2)
             generated_token = torch.multinomial(output.squeeze(), 1)[0]
             #print generated_token.size(), type(generated_token)
             generated_word = corpus.dictionary.idx2word[int(generated_token.squeeze())]
             gen_questions.append(generated_word)
             new_input_token = Variable(torch.LongTensor(1,1).fill_(int(generated_token.squeeze()))).cuda()
-
-            ###ARGMAX
-            """
-            _, generated_token = torch.max(output.detach(), 2)
-            generated_token = int(generated_token.data[0])
-            generated_word = corpus.dictionary.idx2word[generated_token]
-            new_input_token = Variable(torch.LongTensor(1,1).fill_(generated_token)).cuda()
-            input_token = corpus.dictionary.idx2word[generated_token]
-            gen_questions.append(generated_word)"""
             if generated_word == "<eos>":
                 break
-            elif len(gen_questions)==25: 
+            elif len(gen_questions)==25:
                 break
 
         print ' '.join(original_questions)+"\t\t"+' '.join(gen_questions)
-
-        #hidden = model.init_hidden(data.size(0))
     return None
-
 
 # Loop over epochs.
 lr = args.lr
@@ -135,7 +130,7 @@ with open(args.save, 'rb') as f:
     model = torch.load(f)
     # after load the rnn params are not a continuous chunk of memory
     # this makes them a continuous chunk, and will speed up forward pass
-    model.rnn.flatten_parameters()
+    #model.rnn.flatten_parameters()
 
 # Run on test data.
 
