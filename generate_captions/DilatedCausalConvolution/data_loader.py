30ec3eb6a08cb0a6c4d98aac3edebebd7edd8270
saikrishnarallabandi <srallaba@cs.cmu.edu>
2018-12-31 10:44:02 -0500

    Added dilated residual conv module

---
+++ generate_captions/DilatedCausalConvolution/data_loader.py
@@ -0,0 +1,154 @@
+import torch
+import torchvision.transforms as transforms
+import torch.utils.data as data
+import os, sys
+import pickle
+import numpy as np
+import nltk
+from build_vocab import Vocabulary
+import argparse
+
+
+class CocoDataset(data.Dataset):
+    """COCO Custom Dataset compatible with torch.utils.data.DataLoader."""
+    def __init__(self, i2f_dict, i2c_dict, vocab, transform=None):
+        """Set the path for images, captions and vocabulary wrapper.
+        
+        Args:
+            i2f_dict: image id to feature map
+            i2c_dict: image id to caption map
+            vocab: vocabulary wrapper.
+            transform: image transformer.
+        """
+        self.ids = i2f_dict.keys()
+        self.image_name = []
+        self.features = []
+        self.captions = []
+
+        for i, id in enumerate(self.ids):
+            self.image_name.append(id)
+            self.features.append(i2f_dict[id])
+            self.captions.append(i2c_dict[id])
+        
+            if (i % 1000) == 0:
+                print("[{}/{}] curated.".format(i+1, len(self.ids)))
+
+        self.vocab = vocab
+
+    def __getitem__(self, index):
+        """Returns one data pair (image and caption)."""
+        vocab = self.vocab
+        caption = self.captions[index]
+        img_feature = self.features[index]
+        img_name = self.image_name[index]
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(str(caption).lower())
+        caption = []
+        caption.append(vocab('<start>'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('<end>'))
+        target = torch.Tensor(caption)
+        image = torch.Tensor(img_feature)
+        return image, target, img_name
+
+    def __len__(self):
+        return len(self.ids)
+
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (image, caption, image_name).
+    
+    We should build custom collate_fn rather than using default collate_fn, 
+    because merging caption (including padding) is not supported in default.
+    Args:
+        data: list of tuple (image, caption, image_name). 
+            - image: torch tensor of shape (features_dim).
+            - caption: torch tensor of shape (?); variable length.
+    Returns:
+        images: torch tensor of shape (batch_size, features_dim).
+        targets: torch tensor of shape (batch_size, padded_length).
+        lengths: list; valid length for each padded caption.
+    """
+    # Sort a data list by caption length (descending order).
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions, image_names = zip(*data)
+
+    images = torch.stack(images, 0)
+
+    # Merge captions (from tuple of 1D tensor to 2D tensor).
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]        
+    
+    return images, targets, lengths, image_names
+
+
+def get_loader(i2f_dict, i2c_dict, vocab, transform, batch_size, shuffle, num_workers):
+    """Returns torch.utils.data.DataLoader for custom coco dataset."""
+    # COCO caption dataset
+    coco = CocoDataset(i2f_dict=i2f_dict,
+                       i2c_dict=i2c_dict,
+                       vocab=vocab,
+                       transform=transform)
+    
+    # Data loader for COCO dataset
+    # This will return (images, captions, lengths, image_names) for each iteration.
+    # images: a tensor of shape (batch_size, feature_lenght).
+    # captions: a tensor of shape (batch_size, padded_length).
+    # lengths: a list indicating valid length for each caption. length is (batch_size).
+    data_loader = torch.utils.data.DataLoader(dataset=coco, 
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              num_workers=num_workers,
+                                              collate_fn=collate_fn)
+    return data_loader
+
+
+def main(args):
+    with open(args.vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
+
+    with open(args.imgid2caption_pickle_file, 'rb') as f:
+        imageid2captions = pickle.load(f)
+
+    with open(args.imgid2feature_pickle_file, 'rb') as f:
+        imageid2features = pickle.load(f)
+
+    data_loader = get_loader(i2f_dict=imageid2features,
+                             i2c_dict=imageid2captions, 
+                             vocab=vocab, 
+                             transform=None, 
+                             batch_size=args.batch_size, 
+                             shuffle=True, 
+                             num_workers=2)
+
+    for i, data_item in enumerate(data_loader): 
+        img_features, cap_features, lens, names = data_item
+        print(img_features.size())
+        print(cap_features.size())
+        print(lens)
+        print(names)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--imgid2caption_pickle_file', type=str,
+                        default='./imageid2captions.pkl',
+                        help='path for image id 2 captions file')
+
+    parser.add_argument('--imgid2feature_pickle_file', type=str,
+                        default='./imageid2features.pkl',
+                        help='path for image id 2 features file')
+
+    parser.add_argument('--vocab_path', type=str, default='./vocab.pkl',
+                        help='path for vocabulary file')
+
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help='batch size')
+
+    args = parser.parse_args()
+    main(args)
---
+++ generate_captions/DilatedCausalConvolution/model.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+import sys
+from layers import *
+from modules import *
+import numpy as np
+
+class CaptionRNN(nn.Module):
+    def __init__(self, feature_size, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20):
+        """Set the hyper-parameters and build the layers."""
+        super(CaptionRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.max_seg_length = max_seq_length
+
+        self.image_linear = nn.Linear(feature_size, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+
+        
+    def forward(self, features, captions, lengths):
+        """Decode image feature vectors and generates captions."""
+        features = self.bn(self.image_linear(features))
+
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
+        hiddens, _ = self.lstm(packed)
+        outputs = self.linear(hiddens[0])
+        return outputs
+    
+    def sample(self, features, states=None):
+        """Generate captions for given image features using greedy search."""
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(self.max_seg_length):
+            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
+            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
+            _, predicted = outputs.max(1)                        # predicted: (batch_size)
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
+            inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+        sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_seq_length)
+        return sampled_ids
+
+class CaptionCNN(nn.Module):
+    def __init__(self, feature_size, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20):
+        """Set the hyper-parameters and build the layers."""
+        super(CaptionCNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        
+        layers = 12
+        stacks = 4
+        layers_per_stack = layers // stacks
+        self.kernel_size = 3
+        self.stride = 1
+        self.vocab_size = vocab_size
+           
+
+        self.conv_modules = nn.ModuleList()
+        for layer in range(layers):
+            dilation = 2**(layer % layers_per_stack)
+            self.padding = int((self.kernel_size - 1) * dilation)
+            conv = residualconvmodule(embed_size,embed_size, self.kernel_size, self.stride, self.padding,dilation)
+            self.conv_modules.append(conv)
+
+
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.max_seg_length = max_seq_length
+
+        self.image_linear = nn.Linear(feature_size, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+        
+        self.final_fc1 = SequenceWise(nn.Linear(256, 512))
+        self.final_fc2 = SequenceWise(nn.Linear(512, vocab_size))
+
+
+        
+    def forward(self, features, captions, lengths):
+        """Decode image feature vectors and generates captions."""
+
+        features = self.bn(self.image_linear(features))
+
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+
+        c = None
+        x = embeddings.transpose(1,2)
+        for module in self.conv_modules:
+          x = F.relu(module(x, c))
+        x = x.transpose(1,2)
+
+        x = F.relu(self.final_fc1(x))
+        x = self.final_fc2(x)
+
+        return x[:,:-1,:]
+    
+    def clear_buffers(self):
+
+       for module in self.conv_modules:
+           module.clear_buffer()
+
+    
+    def sample(self, features, states=None):
+        """Generate captions for given image features using greedy search."""
+
+        outputs = []
+        features = self.bn(self.image_linear(features))
+        inputs = features.unsqueeze(1)
+        bsz = inputs.shape[0]
+        
+        self.clear_buffers()
+        c = None
+        x = inputs
+        for i in range(self.max_seg_length):
+            module_count = 0
+            for module in self.conv_modules:
+                module_count += 1
+                #print("  Module: Feeding into the module number: ", module_count)
+                x = F.relu(module.incremental_forward(x, c))
+            x = F.relu(self.final_fc1(x))
+            x = self.final_fc2(x)
+            #print("Shape of x", x.shape)
+
+            v, predicted = torch.max(x,2)
+            outputs.append(predicted[0])
+            x = self.embed(predicted)
+            #print(predicted, predicted.shape, v)
+            
+        outputs = torch.stack(outputs, 1)
+        return outputs
+
+class CaptionSingleCNN(nn.Module):
+    def __init__(self, feature_size, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20):
+        """Set the hyper-parameters and build the layers."""
+        super(CaptionSingleCNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        
+        layers_per_stack = layers // stacks
+        self.kernel_size = 3
+        self.stride = 1
+        self.vocab_size = vocab_size
+        
+
+        self.conv_modules = nn.ModuleList()
+        for layer in range(layers):
+            dilation = 2**(layer % layers_per_stack)
+            self.padding = int((self.kernel_size - 1)/2 * dilation)
+            conv = residualconvmodule(embed_size,embed_size, self.kernel_size, self.stride, self.padding,dilation)
+            self.conv_modules.append(conv)
+
+
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.max_seg_length = max_seq_length
+
+        self.image_linear = nn.Linear(feature_size, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+        
+        self.final_fc1 = SequenceWise(nn.Linear(256, 512))
+        self.final_fc2 = SequenceWise(nn.Linear(512, vocab_size))
